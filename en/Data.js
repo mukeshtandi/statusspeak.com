de2b3cfb310e1d748b20c922767dcb264d760b13
MUKESH TANDI <42092980+mukeshtandi@users.noreply.github.com>
2022-12-22 06:13:18 +0530

    Update Data.js

--- en/Data.js
+++ en/Data.js
@@ -9,9 +9,21 @@ const name = decodeURIComponent(getQueryParam("n").replace(/-|@|%40|\+|%20/g, " 
 
 	blankname = "Your Name",
 	timeCounterDays = ["Dec 25, 2022", "days", "hours", "min", "sec before"],
-	marquee = ['<img src="../side1.png" style="height:30px; width:30px;" /><br />'],
+	marquee = ['<img src="' + '../side1.png" style="height:30px; width:30px;" /><br />'],
 	mainImgIndex = ["width:50%; height:6%;", "wishing1.gif", "width:85%; height:35%;", "christmas2020d.gif"],
 	mainImgShare = ["width:50%; height:6%;", "2020wish1.png", "width:  85%; height:35%;", "ch2022c.png"],
 	sharingButtonName = ["Share with friends", "Close"],
 	sharewpmobi = "🤗😇 Have you seen this??? %0A*" + name + "* sent you a surprise message %0A💁 *Open this*%0A👇👇🏻👇👇 %0A statusspeak.com/wow/?n=",
 	sharefbmobi = "https%3A%2F%2Fstatusspeak.com/wow/?n=",
+
+//Index Page Images
+$('#wish1').attr('src','chenwish1.gif');
+$('#main1').attr('src','chenmain1.gif');
+$('#under1').attr('src','santaji6.gif');
+$('#under2').attr('src','santaji2.gif');
+
+//Share Page Images
+$('#wish2').attr('src','chenwish1.gif');
+$('#main2').attr('src','chenmain1.gif');
+$('#under3').attr('src','santaji6.gif');
+$('#under4').attr('src','santaji2.gif');
