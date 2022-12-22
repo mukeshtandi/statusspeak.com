697ddf6d390a152c20e2c813e3948f2a9bed4154
MUKESH TANDI <42092980+mukeshtandi@users.noreply.github.com>
2022-12-22 07:33:09 +0530

    Update Data.js

--- en/Data.js
+++ en/Data.js
@@ -1,15 +1,15 @@
 function getQueryParam(e){var a=window.location.search.match(new RegExp("(\\?|&)"+e+"(\\[\\])?=([^&]*)"));return!!a&&a[3]};
 const name = decodeURIComponent(getQueryParam("n").replace(/-|@|%40|\+|%20/g, " ")),
-	picwtsp = url + "mukeshtandi/statusspeak.com/wtsp.png",
-	fbpic = url + "mukeshtandi/statusspeak.com/fb.png",
-	messengerpic = url + "mukeshtandi/statusspeak.com/messenger.png",
+	picwtsp = "https://cdn.jsdelivr.net/gh/mukeshtandi/statusspeak.com/wtsp.png",
+	fbpic = "https://cdn.jsdelivr.net/gh/mukeshtandi/statusspeak.com/fb.png",
+	messengerpic = "https://cdn.jsdelivr.net/gh/mukeshtandi/statusspeak.com/messenger.png",
 	fbID = "413498309346615",
 	wpbtnName = "Share on Whatsapp",
 	FbLinkBlockExtraText = "";
 
 	blankname = "Your Name",
 	timeCounterDays = ["Dec 25, 2022", "days", "hours", "min", "sec before"],
-	marquee = ['<img src="' + url + 'mukeshtandi/statusspeak.com/en/sas1.png" height="30px" width="30px" /><br /><br /><img src="' + url + 'mukeshtandi/statusspeak.com/en/sas2.png" height="30px" width="30px" /><br /><br /><img src="' + url + 'mukeshtandi/statusspeak.com/en/sas3.png" height="30px" width="30px" /><br /><br /><img src="' + url + 'mukeshtandi/statusspeak.com/en/sas4.png" height="30px" width="30px" /><br /><br /><img src="' + url + 'mukeshtandi/statusspeak.com/en/sas5.png" height="30px" width="30px" /><br /><br /><img src="' + url + 'mukeshtandi/statusspeak.com/en/sas6.png" height="30px" width="30px" /><br /><br /><img src="' + url + 'mukeshtandi/statusspeak.com/en/sas7.png" height="30px" width="30px" /><br /><br /><img src="' + url + 'mukeshtandi/statusspeak.com/en/sas8.png" height="30px" width="30px" /><br /><br />'],
+	marquee = ['<img src="https://cdn.jsdelivr.net/gh/mukeshtandi/statusspeak.com/en/sas1.png" height="30px" width="30px" /><br /><br /><img src="https://cdn.jsdelivr.net/gh/mukeshtandi/statusspeak.com/en/sas2.png" height="30px" width="30px" /><br /><br /><img src="https://cdn.jsdelivr.net/gh/mukeshtandi/statusspeak.com/en/sas3.png" height="30px" width="30px" /><br /><br /><img src="https://cdn.jsdelivr.net/gh/mukeshtandi/statusspeak.com/en/sas4.png" height="30px" width="30px" /><br /><br /><img src="https://cdn.jsdelivr.net/gh/mukeshtandi/statusspeak.com/en/sas5.png" height="30px" width="30px" /><br /><br /><img src="https://cdn.jsdelivr.net/gh/mukeshtandi/statusspeak.com/en/sas6.png" height="30px" width="30px" /><br /><br /><img src="https://cdn.jsdelivr.net/gh/mukeshtandi/statusspeak.com/en/sas7.png" height="30px" width="30px" /><br /><br /><img src="https://cdn.jsdelivr.net/gh/mukeshtandi/statusspeak.com/en/sas8.png" height="30px" width="30px" /><br /><br />'],
 	mainImgIndex = [""],
 	mainImgShare = [""],
 	sharingButtonName = ["Share with friends", "Close"],
@@ -17,13 +17,13 @@ const name = decodeURIComponent(getQueryParam("n").replace(/-|@|%40|\+|%20/g, " 
 	sharefbmobi = "https%3A%2F%2Fstatusspeak.com/wow/?n=",
 
 //Index Page Images
-$('#wish1').attr('src', url 'mukeshtandi/statusspeak.com/en/chenwish1.gif');
-$('#main1').attr('src', url 'mukeshtandi/statusspeak.com/en/chenmain1.gif');
-$('#under1').attr('src', url 'mukeshtandi/statusspeak.com/en/santaji6.gif');
-$('#under2').attr('src', url 'mukeshtandi/statusspeak.com/en/santaji2.gif');
+$('#wish1').attr('src','https://cdn.jsdelivr.net/gh/mukeshtandi/statusspeak.com/en/chenwish1.gif');
+$('#main1').attr('src','https://cdn.jsdelivr.net/gh/mukeshtandi/statusspeak.com/en/chenmain1.gif');
+$('#under1').attr('src','https://cdn.jsdelivr.net/gh/mukeshtandi/statusspeak.com/en/santaji6.gif');
+$('#under2').attr('src','https://cdn.jsdelivr.net/gh/mukeshtandi/statusspeak.com/en/santaji2.gif');
 
 //Share Page Images
-$('#wish2').attr('src', url 'mukeshtandi/statusspeak.com/en/chenwish1.gif');
-$('#main2').attr('src', url 'mukeshtandi/statusspeak.com/en/chenmain1.gif');
-$('#under3').attr('src', url 'mukeshtandi/statusspeak.com/en/santaji6.gif');
-$('#under4').attr('src', url 'mukeshtandi/statusspeak.com/en/santaji2.gif');
+$('#wish2').attr('src','https://cdn.jsdelivr.net/gh/mukeshtandi/statusspeak.com/en/chenwish1.gif');
+$('#main2').attr('src','https://cdn.jsdelivr.net/gh/mukeshtandi/statusspeak.com/en/chenmain1.gif');
+$('#under3').attr('src','https://cdn.jsdelivr.net/gh/mukeshtandi/statusspeak.com/en/santaji6.gif');
+$('#under4').attr('src','https://cdn.jsdelivr.net/gh/mukeshtandi/statusspeak.com/en/santaji2.gif');
